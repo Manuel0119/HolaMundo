00fe9c5ae4216dd94dacbc5ebb384254191d679a
daw1 <daw1@IS31WX04>
2022-02-22 09:32:34 +0100

    Modificando Hola Mundo 2.0

--- src/main/java/paquete/HolaMundo.java
+++ src/main/java/paquete/HolaMundo.java
@@ -15,7 +15,7 @@ public class HolaMundo {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Hola Mundo modificado");
+        System.out.println("Modificando la rama de HolaMundo");
     }
     
 }
